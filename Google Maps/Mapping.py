2b23a0a23b98673c61e51c1153e99613656b1a06
plovanpete <plovanpete@gmail.com>
2023-09-23 10:11:29 -0500

    Create Mapping.py
    map template

---
+++ Google Maps/Mapping.py
@@ -0,0 +1,29 @@
+import googlemaps
+
+# Google API Key used for Google Maps!
+GOOGLE_API_KEY = 'AIzaSyBRrTFJpK4ZqpjHwMF45OUdwwhRH0D_e5Y'
+
+def geocaching():
+    return
+
+def getGoogleMapsURL():
+    return
+
+def getGoogleMaps():
+    return
+
+def displayMap():
+    return
+
+def increaseZoom():
+    return
+
+def decreaseZoom():
+    return
+
+def createMarker():
+    return
+
+def createDeals():
+    return
+
