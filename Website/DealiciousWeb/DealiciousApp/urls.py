1ed1640aba59e782ba9fd54959ecf458a94ce570
JassMaster <145850027+JassMaster@users.noreply.github.com>
2023-09-24 09:16:00 -0500

    Add files via upload

--- Website/DealiciousWeb/DealiciousApp/urls.py
+++ Website/DealiciousWeb/DealiciousApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.DealiciousView )
+    path('', views.DealiciousView, name='home'),
+    path('Report/', views.ReportView, name='report'),
 ]
--- Website/DealiciousWeb/DealiciousApp/views.py
+++ Website/DealiciousWeb/DealiciousApp/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def DealiciousView(request):
     return render(request, "Dealicious.html")
+def ReportView(request):
+    return render(request, "Report.html")
