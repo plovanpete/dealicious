6a90f6d345d21e3e9c0eb6538bfce361d199703a
plovanpete <plovanpete@gmail.com>
2023-09-23 23:42:58 -0500

    Mass delete testfailed

--- Python/Website/Django/mysite/DealTestSite/DealProject/AppDeals/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppdealsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AppDeals'
--- Python/Website/Django/mysite/DealTestSite/DealProject/AppDeals/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-# Create your views here.
-urlpatterns =[
-    path('myview/', views.myview)
-]
